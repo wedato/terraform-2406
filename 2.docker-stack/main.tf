448766c085a91e1accea20fdd1bcf227ea3d7d8e
bngams <n.boris@devidia.net>
2024-06-06 13:17:36 +0000

    move variables to specific config file variable.tf, and use variables with *.tfvars files or *.auto.tfvars files (not in SCM)

--- 2.docker-stack/main.tf
+++ 2.docker-stack/main.tf
@@ -3,20 +3,6 @@
 # 2. volumes 
 ##########
 
-variable "db_vars" {
-  type = map(string)
-  # if no default value => required (to initialize before pla/apply...) 
-  default = {
-    "MYSQL_HOSTNAME" = "db",
-    "MYSQL_ROOT_PASSWORD" = "wordpressroot"
-    "MYSQL_DATABASE" = "wordpressdb"
-    "MYSQL_USER" = "wordpressdbu"
-    "MYSQL_PASSWORD" = "wordpressuser"
-  }
-  # declare as a secret ("secret like") 
-  sensitive = true
-}
-
 ##########
 # 1. networks
 ##########
