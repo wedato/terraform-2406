8fa7839fbce10cf5950e0f5d101655ba9899f05c
bngams <n.boris@devidia.net>
2024-06-07 09:09:09 +0000

    some todos notes to handle variables in a better way in our modules

--- 3.docker-stack-modules/modules/images/variables.tf
+++ 3.docker-stack-modules/modules/images/variables.tf
@@ -1,5 +1,7 @@
 # if no default value => not mandatory 
 variable "images_names" {
+  # TODO: refacto as map (key => value) 
+  # to handle it in a more generic ways in outputs
   type    = list(string)
   description = "The names of all images to DL"
   # no default value => required 
--- 3.docker-stack-modules/modules/volumes/variables.tf
+++ 3.docker-stack-modules/modules/volumes/variables.tf
@@ -1,5 +1,7 @@
 # if no default value => not mandatory 
 variable "volumes_names" {
+  # TODO: refacto as map (key => value) 
+  # to handle it in a more generic ways in outputs 
   type    = list(string)
   description = "The names of all volumes to create"
   # no default value => required 
